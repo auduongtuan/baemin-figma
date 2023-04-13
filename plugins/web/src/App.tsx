da25c35de528d4f27cab15f344c023cac694d8c7
Tuan Au Duong <audtuan@gmail.com>
2023-04-13 09:56:13 +0700

    Test i18n js

--- plugins/web/src/App.tsx
+++ plugins/web/src/App.tsx
@@ -1,33 +1,43 @@
 import { useState } from 'react'
-import i18next from 'i18next';
 // import './App.css'
 import "ds/ui.scss";
-import { initReactI18next } from "react-i18next";
 import en from './i18n/en.json';
 import vi from './i18n/vi.json';
-import { useTranslation, Trans } from "react-i18next";
-i18next.use(initReactI18next).init({
-  lng: 'en', // if you're using a language detector, do not define the lng option
-  debug: true,
-  defaultNS: 'translations',
-  resources: {
-    en: {
-      translations: en
-    },
-    vi: {
-      translations: vi
-    }
-  },
+import { I18n } from "i18n-js";
+
+// import i18next from 'i18next';
+// import { initReactI18next } from "react-i18next";
+// import { useTranslation, Trans } from "react-i18next";
+// i18next.use(initReactI18next).init({
+//   lng: 'en', // if you're using a language detector, do not define the lng option
+//   debug: true,
+//   defaultNS: 'translations',
+//   resources: {
+//     en: {
+//       translations: en
+//     },
+//     vi: {
+//       translations: vi
+//     }
+//   },
+// });
+const i18n = new I18n({
+  en,
+  vi
 });
+
+
+i18n.defaultLocale = "en";
 
 import {Button} from 'ds';
 function App() {
-  const { t, i18n } = useTranslation();
+  // const { t, i18n } = useTranslation();
   return (
     <div className="App">
-      <Button variant="primary">{t('common.dish_with_count', {count: 1})}</Button>
+      {/* <Button variant="primary">{t('common.dish_with_count', {count: 1})}</Button>
       <Button>{t("common.back")}</Button>
-      <Button>{t("welcome", {name: 'Test'})}</Button>
+      <Button>{t("welcome", {name: 'Test'})}</Button> */}
+      {i18n.t('translation.dish_with_count_test', {count: 2})}
     </div>
   )
 }
